48a3429d380e17e0b10388095f26272ca5c12ff9
Tim Morgan <tim@squareup.com>
2013-03-19 14:40:20 -0700

    Remove attempt to load system_timer gem
    The find_by_name method doesn't exist in all versions of RubyGems, and
    I think the call to require should be sufficient anyway.

--- lib/squash/ruby.rb
+++ lib/squash/ruby.rb
@@ -18,9 +18,9 @@ require 'net/https'
 
 require 'json'
 begin
-  Gem::Specification.find_by_name('system_timer')
   require 'system_timer'
-rescue Gem::LoadError
+rescue LoadError
+  # optional
 end
 
 # Container for methods relating to notifying Squash of exceptions.
